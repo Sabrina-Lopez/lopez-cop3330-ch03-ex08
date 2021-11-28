6b198d3cf11c88c3fb97aaf3849d1a53f7b1c8a4
Berrybunny015 <berrybunny015@gmail.com>
2021-11-28 14:13:45 -0500

    file changes

---
+++ lopez-cop3330-ch03-ex08.h
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Sabrina Lopez
+ */
+
+#include <iostream>
+
+int main() {
+    int userNum; //declare userNum, an integer variable to hold the user's inputted numerical value
+
+    //tell the user to input a value to determine if it is odd or even
+    std::cout << "Please input a number. \n";
+
+    //take the user's inputted value
+    std::cin >> userNum;
+
+    if(abs(userNum) % 2 == 0) { //if the user's inputted value is divisible by 2, then it is even
+        std::cout << "The value " << userNum << " is an even number.\n"; //output to the user that their value is even
+    }
+    else { //if not, then it is odd
+        std::cout << "The value " << userNum << " is an odd number.\n"; //output to the user that their value is odd
+    }
+
+    return 0;
+}
